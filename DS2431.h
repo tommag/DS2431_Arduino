993d9fa1ef2ccb323cb388104b0516178befccba
hitech95 <nicveronese@gmail.com>
2018-10-31 18:28:46 +0100

    Add support for Skip ROM and different Write buffer length
    If the Serial Number is not passed we use Skip ROM command to
    communicate. (This work only if a single slave is present)
    
    We also support a write buffer with size up to 8 bytes.
    Now you can write a single byte if needed.
    
    Now we can skip the READ verification process. If the PF bit fail we
    force the verify process.
    If the verify process fail we attempt to read again the scratchpad.

--- DS2431.h
+++ DS2431.h
@@ -2,6 +2,7 @@
 MIT License
 
 Copyright (c) 2017 Tom Magnier
+Modified 2018 by Nicolò Veronese
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -29,32 +30,46 @@ SOFTWARE.
 
 class DS2431 {
 public:
+  const static uint8_t ONE_WIRE_MAC_SIZE = 8;
+
   const static uint8_t ONE_WIRE_FAMILY_CODE = 0x2D;
 
+  const static uint8_t DS2431_EEPROM_SIZE = 128;
+  const static uint8_t DS2431_ROW_SIZE = 8;
+
   DS2431(OneWire &ow); // OneWire class
-	
-  void begin(uint8_t serialNumber[8]); // family code, 48bit serial number and CRC as returned by OneWire search function
+
+  void begin(uint8_t serialNumber[ONE_WIRE_MAC_SIZE]); // family code, 48bit serial number and CRC as returned by OneWire search function
 
   /* Single byte read
   */
-  int read(uint16_t address);
+  uint8_t read(uint16_t address);
 
   /* Multiple byte read.
   */
-  bool read(uint16_t address, uint8_t *buf, uint16_t len);
+  void read(uint16_t address, uint8_t *buf, uint16_t len);
 
-  /* Full row (8 byte) write. Please note : address must be a multiple of 8.
-  Return true if operation was successful.
-  The OneWire bus should be de-powered after calling this function.
+  /* Multiple byte write.
+    Please note : address must be a multiple of 8. Write up to 8 bytes
+    Return true if operation was successful.
+    The OneWire bus should be de-powered after calling this function.
   */
-  bool write(uint16_t address, uint8_t buf[8]);
+  bool write(uint16_t address, const uint8_t *buf, uint16_t count, bool verify = false);
 
 private:
-  OneWire &_ow;
-  uint8_t _serialNumber[8];
-  bool _init;
+  const static uint8_t DS2431_PF_MASK = 0x07;
+  const static uint8_t DS2431_WRITE_MASK = 0xAA;
 
-  const static uint16_t EEPROM_SIZE = 128;
+  const static uint8_t DS2431_CMD_SIZE = 3;
+  const static uint8_t DS2431_CRC_SIZE = 2;
+
+  const static uint8_t DS2431_READ_RETRY = 2;
+
+  const static uint16_t DS2431_BUFFER_SIZE = DS2431_ROW_SIZE + DS2431_CMD_SIZE + DS2431_CRC_SIZE;
+
+  OneWire &_ow;
+  uint8_t _serialNumber[ONE_WIRE_MAC_SIZE];
+  bool _skiprom;
 
   enum Commands {
     WRITE_SCRATCHPAD = 0x0F,
@@ -62,6 +77,17 @@ private:
     COPY_SCRATCHPAD = 0x55,
     READ_MEMORY = 0xF0
   };
+
+  bool _write(uint16_t address, const uint8_t *buf, uint16_t count, bool verify);
+
+  inline void _startTransmission()
+  {
+    _ow.reset();
+    if (_skiprom)
+    _ow.skip();
+    else
+    _ow.select(_serialNumber);
+  }
 };
 
 #endif // _DS2431_H
